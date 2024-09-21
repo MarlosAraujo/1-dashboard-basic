8f81a3d1f40becb5dd7329c6ba615b8b23de2c84
root <mmarlos@hotmail.com>
2024-09-21 19:28:40 -0300

    feat: add list product

---
+++ src/app/_helpers/product/get-product.ts
@@ -0,0 +1,16 @@
+import "server-only";
+
+import { Product } from "@prisma/client";
+import { db } from "../../_lib/prisma";
+
+export interface ProductDto extends Product {
+  status: "IN_STOCK" | "OUT_OF_STOCK";
+}
+
+export const getProducts = async (): Promise<ProductDto[]> => {
+  const products = await db.product.findMany({});
+  return products.map((product) => ({
+    ...product,
+    status: product.stock > 0 ? "IN_STOCK" : "OUT_OF_STOCK",
+  }));
+};
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,5 +1,13 @@
+import { Button } from "../_components/ui/button";
+import { PlusIcon } from "lucide-react";
+import { DataTable } from "../_components/ui/data-table";
+import { getProducts } from "../_helpers/product/get-product";
+import { productTableColumns } from "./__components/table-columns";
 
 const ProductsPage = async () => {
+
+  const products = await getProducts();
+
   return (
     <div className="m-8 w-full space-y-8 rounded-lg bg-white p-8">
     <div className="flex w-full items-center justify-between">
@@ -10,8 +18,14 @@ const ProductsPage = async () => {
         <h2 className="text-xl font-semibold">Produtos</h2>
       </div>
       {/* <AddProductButton /> */}
+      <div className="gap-2">
+      <Button>
+        <PlusIcon size={20}/>
+        Novo Produto
+      </Button>
+      </div>
     </div>
-    {/* <DataTable columns={productTableColumns} data={products} /> */}
+    <DataTable columns={productTableColumns} data={products} />
   </div>
   );
 };
